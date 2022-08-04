86179607d36e672c725249f4d6b4ff7b7c9200f2
Vadim <vadim_kimm@icloud.com>
2022-08-04 20:37:29 +0300

    Created protocol Displayable

---
+++ HW25-Alamofire/Sources/Scenes/Protocols/Displayable.swift
@@ -0,0 +1,13 @@
+//
+//  Displayable.swift
+//  HW25-Alamofire
+//
+//  Created by Vadim Kim on 04.08.2022.
+//
+
+import Foundation
+
+protocol Displayable {
+    var cardNameLabelText: String { get }
+    var manaCostLabelText: String { get }
+}
