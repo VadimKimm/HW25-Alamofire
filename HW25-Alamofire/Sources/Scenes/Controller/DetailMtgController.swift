8e4db543c48621582635290a37fe9c7f785364e6
Vadim <vadim_kimm@icloud.com>
2022-08-04 22:02:34 +0300

    Created class DetailMtgController

--- HW25-Alamofire/Sources/Scenes/Controller/DetailMtgController.swift
+++ HW25-Alamofire/Sources/Scenes/Controller/DetailMtgController.swift
@@ -5,4 +5,32 @@
 //  Created by Vadim Kim on 04.08.2022.
 //
 
-import Foundation
+import UIKit
+
+class DetailMtgController: UIViewController {
+
+    //MARK: - Properties -
+
+    var card: Displayable?
+
+    private var detailMtgView: DetailMtgView? {
+        guard isViewLoaded else { return nil }
+        return view as? DetailMtgView
+    }
+
+    //MARK: - Lifecycle -
+
+    override func loadView() {
+        super.loadView()
+        view = DetailMtgView()
+    }
+
+    override func viewDidLoad() {
+      super.viewDidLoad()
+      commonInit()
+    }
+
+    private func commonInit() {
+        detailMtgView?.configure(with: card)
+    }
+}
